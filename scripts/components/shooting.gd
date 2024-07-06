7741590d8f3217fbdaedda8abfe74f1c1d8199a1
unknown <you@example.com>
2024-07-06 16:27:21 +0300

    trying to make shooting

---
+++ scripts/components/shooting.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+func shoot(target_direction : Vector2):
+	pass
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 const SPEED : float = 200
+const STOP_FRAMES : float = 5
 const JUMP_VELOCITY : float = -400
 const COYOTE_TIME : float = 0.1
 
@@ -10,6 +11,8 @@ const COYOTE_TIME : float = 0.1
 @onready var health_component : HealthComponent = $HealthComponent
 
 var gravity : float = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var look_direction : Vector2 = Vector2(0, 0)
 
 func activate_enemy(area : Area2D, activate : bool):
 	var parent_node = area.get_parent() 
@@ -24,15 +27,23 @@ func agro_enemy(area : Area2D):
 	if is_instance_of(parent_node, Enemy):
 		parent_node.attack_player()
 
+func get_look_direction() -> Vector2:
+	var new_direction : Vector2 = Input.get_vector("left", "right", "down", "up")
+	if new_direction.length_squared() == 0: 
+		return Vector2(look_direction.x, 0)
+	return new_direction.normalized()
+
 func _ready():
 	activation_area.area_entered.connect(func(area : Area2D): activate_enemy(area, true))
 	activation_area.area_exited.connect(func(area : Area2D): activate_enemy(area, false))
 	agro_area.area_entered.connect(agro_enemy)
 
 func _process(_delta):
+	_gravity()
 	_move()
+	look_direction = get_look_direction()
 
-func _move():
+func _gravity():
 	## Apply gravity
 	if not is_on_floor():
 		if sign(velocity.y) == -1:
@@ -42,7 +53,8 @@ func _move():
 				velocity.y += gravity
 		else:
 			velocity.y += gravity / 2
-	
+
+func _move():
 	## Jump and fall logic
 	if Input.is_action_just_pressed("jump") and (is_on_floor() or not coyote_timer.is_stopped()):
 		if Input.is_action_pressed("down"):
@@ -51,11 +63,11 @@ func _move():
 			velocity.y = JUMP_VELOCITY
 	
 	## Sideways movement logic
-	var direction = Input.get_axis("left", "right")
+	var direction : float = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED / STOP_FRAMES)
 	
 	## Apply velocity vector and update coyote timer
 	move_and_slide()
