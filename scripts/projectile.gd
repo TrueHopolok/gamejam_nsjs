9f0eb6f1a919d3c39018dab766e1e4a6a9b7c8aa
unknown <you@example.com>
2024-07-06 14:57:43 +0300

    Renaming variable

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -3,13 +3,13 @@ class_name Projectile extends Area2D
 const SPEED : float = 3
 
 var damage : float = 1.0
-var velocity : Vector2
+var direction : Vector2
 
 func _ready():
 	area_entered.connect(func(_area : Area2D): queue_free())
 	$VisibleOnScreenNotifier2D.screen_exited.connect(queue_free)
-	velocity *= SPEED
-	print(damage, velocity)
+	direction *= SPEED
+	print(damage, direction)
 
 func _process(_delta):
-	global_position += velocity
+	global_position += direction
